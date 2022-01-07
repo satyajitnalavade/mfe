fc883dcb1e03bed959c7d2e9e74e10ecaa2fbf9c
satyajitn <satyajit.nalavade@gmail.com>
2022-01-06 20:53:07 -0600

    Initial Commit

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,28 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const commonConfig = require("./webpack.common");
+const packageJSON = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  output: {
+    publicPath: "http://localhost:8081/",
+  },
+  devServer: {
+    port: 8081,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: packageJSON.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
