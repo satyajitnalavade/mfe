708ed25ce7bdbc19c009f176cd0aebed418244ef
satyajitn <satyajit.nalavade@gmail.com>
2022-01-09 18:46:57 -0600

    Added Lazy loading of marketing and auth modules

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
 import Header from './components/Header';
+import Progress from './components/Progress';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 
 const generateClassName = createGenerateClassName({
@@ -18,12 +20,14 @@ export default () => {
     <StylesProvider generateClassName={generateClassName}>
     <div>
       <Header />
+      <Suspense fallback={<Progress />}>
       <Switch>
-        <Route path="/auth" component={ AuthApp } />
+        <Route path="/auth" component={ AuthLazy } />
 
-        <Route path="/" component={ MarketingApp } />
+        <Route path="/" component={ MarketingLazy } />
         
       </Switch>
+      </Suspense>
     </div>
     </StylesProvider>
     </BrowserRouter>
