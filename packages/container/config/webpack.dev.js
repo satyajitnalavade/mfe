8d3690da633ceda8980f347df0a8982c6ebcfeba
satyajitn <satyajit.nalavade@gmail.com>
2022-01-10 00:03:14 -0600

    fixed issue with dashboard loading

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,8 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const packageJSON = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -10,19 +11,17 @@ const devConfig = {
   },
   devServer: {
     port: 8080,
-    historyApiFallback: {
-      index: 'index.html',
-    },
+    historyApiFallback: true,
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: "container",
       remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-        auth: 'auth@http://localhost:8082/remoteEntry.js',
-        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        auth: "auth@http://localhost:8082/remoteEntry.js",
+        dashboard: "dashboard@http://localhost:8083/remoteEntry.js",
       },
-      shared: packageJson.dependencies,
+      shared: packageJSON.dependencies,
     }),
   ],
 };
--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,7 +1,9 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
+
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
